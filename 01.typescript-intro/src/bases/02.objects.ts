1fe0c2b79e4156b8b6ff563740eb18fcc6cd45f0
fersayago <sayago.fernando@gmail.com>
2023-01-08 19:12:30 -0300

    objetos e interfaces

---
+++ 01.typescript-intro/src/bases/02.objects.ts
@@ -0,0 +1,15 @@
+export const pokemonIds = [1, 20, 30, 34, 66]
+
+interface Pokemon {
+  id: number
+  name: string
+  age?: number
+}
+
+// la diferencia principal con las clasese es que no se pueden instanciar interfaces
+// const pikachu = new Pokemon() // Error
+
+export const bulbasaur: Pokemon = {
+  id: 1,
+  name: 'Bulbasaur',
+}
--- 01.typescript-intro/src/main.ts
+++ 01.typescript-intro/src/main.ts
@@ -1,8 +1,8 @@
-import { name, age } from './bases/01.types'
-
+// import { name, age } from './bases/01.types'
+// import { pokemonIds } from './bases/02.objects'
+import { bulbasaur } from './bases/02.objects'
 import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <h1>Hello ${ name }</h1>
-  <>edad: ${ age }</
+  <h1>Hello ${ bulbasaur.name }!</h1>
 `
