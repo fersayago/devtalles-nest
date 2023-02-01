05d8df3ebbbee3972ef0158c853b4f0ef49ff18b
fersayago <sayago.fernando@gmail.com>
2023-02-01 16:00:08 -0300

    Getters, métodos y THIS

--- 01.typescript-intro/src/bases/03.classes.ts
+++ 01.typescript-intro/src/bases/03.classes.ts
@@ -13,10 +13,31 @@
 
 // Definir clases de forma corta
 export class Pokemon {
+
+  // usamos el id para no tener que recibir como parametro la imagen ya que solo varia el numero en la URL. Para esto mismo usamos el getter
+  get imageUrl(): string{ 
+    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${ this.id }.png`
+  }
+
   constructor(
     public id: number,
-    public name: string
+    public name: string,
+    // public imageUrl: string
   ) {}
+
+  // ! METODOS: funciones que tienen acceso a las propiedades y otros metodos
+  public scream() {
+    this.speak()
+    console.log(`${ this.name.toUpperCase() }!!!`)
+  }
+
+  // ! Si defiimos un metodo privado solo se puede usar dentro de la misma clase
+  private speak() {
+    console.log(`${ this.name } ${ this.name }`)
+  }
 }
 
-export const charmander = new Pokemon(4, 'Charmander')
+export const charmander = new Pokemon( 4, 'Charmander' )
+
+//charmander.speak()
+charmander.scream()
