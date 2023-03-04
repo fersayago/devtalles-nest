4d3de53bec730d7c77d2855c987877208e60c8fa
fersayago <sayago.fernando@gmail.com>
2023-03-04 19:35:18 -0300

    S03: Controladores

--- 02.car-dealership/src/app.module.ts
+++ 02.car-dealership/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CarsModule } from './cars/cars.module';
 
 // Los decoradores convierten las clases en objetos que tienen cierta funcionalidad especifica
 @Module({
-  imports: [],
+  imports: [CarsModule],
   controllers: [],
   providers: [],
   exports: [],
