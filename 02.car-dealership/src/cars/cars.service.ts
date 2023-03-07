0fb918912ec3225b4ba656229b8076c4d47351f1
fersayago <sayago.fernando@gmail.com>
2023-03-07 03:45:12 -0300

    S03: Inyección de dependencias

--- 02.car-dealership/src/cars/cars.service.ts
+++ 02.car-dealership/src/cars/cars.service.ts
@@ -19,4 +19,12 @@ export class CarsService {
       model: 'Cherokee',
     },
   ];
+
+  findAll() {
+    return this.cars;
+  }
+
+  findOneById(id: number) {
+    return this.cars.find((car) => car.id === id);
+  }
 }
