425a1d7b418c10e27f84fe0bf17bd43a9ea55354
fersayago <sayago.fernando@gmail.com>
2023-03-17 15:53:21 -0300

    S05: crear CRUD Brands

--- 02.car-dealership/src/app.module.ts
+++ 02.car-dealership/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CarsModule } from './cars/cars.module';
+import { BrandsModule } from './brands/brands.module';
 
 // Los decoradores convierten las clases en objetos que tienen cierta funcionalidad especifica
 @Module({
-  imports: [CarsModule],
+  imports: [CarsModule, BrandsModule],
   controllers: [],
   providers: [],
   exports: [],
