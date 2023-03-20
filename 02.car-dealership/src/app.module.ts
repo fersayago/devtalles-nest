254aa292b4bea072bae30544ab797e5bff046a01
fersayago <sayago.fernando@gmail.com>
2023-03-20 14:51:50 -0300

    S05: Crear servicio seed para cargar datos

--- 02.car-dealership/src/app.module.ts
+++ 02.car-dealership/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CarsModule } from './cars/cars.module';
 import { BrandsModule } from './brands/brands.module';
+import { SeedModule } from './seed/seed.module';
 
 // Los decoradores convierten las clases en objetos que tienen cierta funcionalidad especifica
 @Module({
-  imports: [CarsModule, BrandsModule],
+  imports: [CarsModule, BrandsModule, SeedModule],
   controllers: [],
   providers: [],
   exports: [],
