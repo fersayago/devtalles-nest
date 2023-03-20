e135e598aff305fdfe8f13e8e331ef85fd94e42e
fersayago <sayago.fernando@gmail.com>
2023-03-20 15:13:28 -0300

    S05: Inyectar servicios en otros servicios

--- 02.car-dealership/src/seed/seed.service.ts
+++ 02.car-dealership/src/seed/seed.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { BRANDS_SEED, CARS_SEED } from './data';
+import { CarsService } from '../cars/cars.service';
+import { BrandsService } from '../brands/brands.service';
 
 @Injectable()
 export class SeedService {
+  constructor(
+    private readonly carsService: CarsService,
+    private readonly brandsService: BrandsService,
+  ) {}
   populateDB() {
     // CARS_SEED;
+    this.carsService.fillCarsWithSeedData(CARS_SEED);
+
     // BRANDS_SEED;
+    this.brandsService.fillBrandsWithSeedData(BRANDS_SEED);
 
     return 'SEED excecuted';
   }
