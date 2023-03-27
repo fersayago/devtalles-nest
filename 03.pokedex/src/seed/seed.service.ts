7cafa2b426f3b586abd93576d7ac03efab73849a
fersayago <sayago.fernando@gmail.com>
2023-03-27 03:30:43 -0300

    S08: Insertar registros por lote

--- 03.pokedex/src/seed/seed.service.ts
+++ 03.pokedex/src/seed/seed.service.ts
@@ -1,21 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import axios, { AxiosInstance } from 'axios';
+import { Model } from 'mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
 
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
   async executeSeed() {
     const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=386',
+      'https://pokeapi.co/api/v2/pokemon?limit=1',
     );
 
-    data.results.forEach(({ name, url }) => {
+    data.results.forEach(async ({ name, url }) => {
       const segments: string = url.split('/').at(-2);
       const no: number = +segments;
-      console.log({ name, no });
+      const pokemon = await this.pokemonModel.create({ name, no });
     });
-    return data.results;
+    return 'Seed executed';
   }
 }
