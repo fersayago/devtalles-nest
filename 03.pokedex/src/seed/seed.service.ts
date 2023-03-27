193d497da1f454aad3773b4206d5333ece3f2fba
fersayago <sayago.fernando@gmail.com>
2023-03-27 03:12:53 -0300

    S08: Realizar petición http desde Nest

--- 03.pokedex/src/seed/seed.service.ts
+++ 03.pokedex/src/seed/seed.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  executeSeed() {
-    return 'Seed executed';
+  private readonly axios: AxiosInstance = axios;
+
+  async executeSeed() {
+    const { data } = await this.axios.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=386',
+    );
+
+    data.results.forEach(({ name, url }) => {
+      const segments: string = url.split('/').at(-2);
+      const no: number = +segments;
+      console.log({ name, no });
+    });
+    return data.results;
   }
 }
