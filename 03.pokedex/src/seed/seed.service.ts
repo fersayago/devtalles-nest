9b149a71e12f99848f0fca097a49d10a66191732
fersayago <sayago.fernando@gmail.com>
2023-03-28 14:29:08 -0300

    S08: Insertar multiples registros simultáneamente

--- 03.pokedex/src/seed/seed.service.ts
+++ 03.pokedex/src/seed/seed.service.ts
@@ -14,16 +14,32 @@ export class SeedService {
     private readonly pokemonModel: Model<Pokemon>,
   ) {}
 
+  // borra las bases de datos almacenados y las puebla con los datos extraidos de pokeapi
   async executeSeed() {
+    // Al ejecutar la seed borramos la tabla de pokemons
+    await this.pokemonModel.deleteMany({});
+
+    // Una vez limpia la tabla la poblamos
     const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=1',
+      'https://pokeapi.co/api/v2/pokemon?limit=151',
     );
+
+    const pokemonToInsert: {
+      name: string;
+      no: number;
+    }[] = [];
 
     data.results.forEach(async ({ name, url }) => {
       const segments: string = url.split('/').at(-2);
       const no: number = +segments;
-      const pokemon = await this.pokemonModel.create({ name, no });
+      //const pokemon = await this.pokemonModel.create({ name, no });
+
+      pokemonToInsert.push({ name, no });
     });
+
+    // crea una inserción con un montón de entradas
+    this.pokemonModel.insertMany(pokemonToInsert);
+
     return 'Seed executed';
   }
 }
