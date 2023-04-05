192b5005ab0f618e750c2cb9a21b172aecafef22
fersayago <sayago.fernando@gmail.com>
2023-04-05 14:18:37 -0300

    S10: Get y Delete TypeORM

--- 04.teslo-shop/src/products/products.service.ts
+++ 04.teslo-shop/src/products/products.service.ts
@@ -3,6 +3,7 @@ import {
   Injectable,
   InternalServerErrorException,
   Logger,
+  NotFoundException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -43,20 +44,27 @@ export class ProductsService {
     }
   }
 
+  // TODO: Paginar
   findAll() {
-    return `This action returns all products`;
+    return this.productRepository.find({});
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+    const product = await this.productRepository.findOneBy({ id });
+
+    if (!product)
+      throw new NotFoundException(`Product with id "${id}" not found`);
+
+    return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
+  update(id: string, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    const product = await this.findOne(id);
+    await this.productRepository.remove(product);
   }
 
   private handleDBExceptions(error: any) {
