4db25bf4c9b786339c7ff6f9baab439c9e54c566
fersayago <sayago.fernando@gmail.com>
2023-04-06 04:27:37 -0300

    S10: Buscar por Slug o UUID

--- 04.teslo-shop/src/products/products.service.ts
+++ 04.teslo-shop/src/products/products.service.ts
@@ -11,6 +11,7 @@ import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
 import { Repository } from 'typeorm';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { validate as isUUID } from 'uuid';
 
 @Injectable()
 export class ProductsService {
@@ -55,12 +56,17 @@ export class ProductsService {
     });
   }
 
-  async findOne(id: string) {
-    const product = await this.productRepository.findOneBy({ id });
+  async findOne(term: string) {
+    let product: Product;
+
+    if (isUUID(term)) {
+      product = await this.productRepository.findOneBy({ id: term });
+    } else {
+      product = await this.productRepository.findOneBy({ slug: term });
+    }
 
     if (!product)
-      throw new NotFoundException(`Product with id "${id}" not found`);
-
+      throw new NotFoundException(`Product with term "${term}" not found`);
     return product;
   }
 
