e8a65bb6e2ce8c1e3425d0a8471a30fa18a8984f
fersayago <sayago.fernando@gmail.com>
2023-04-06 04:42:43 -0300

    S10: QueryBuilder

--- 04.teslo-shop/src/products/products.service.ts
+++ 04.teslo-shop/src/products/products.service.ts
@@ -62,7 +62,14 @@ export class ProductsService {
     if (isUUID(term)) {
       product = await this.productRepository.findOneBy({ id: term });
     } else {
-      product = await this.productRepository.findOneBy({ slug: term });
+      // product = await this.productRepository.findOneBy({ slug: term });
+      const queryBuilder = this.productRepository.createQueryBuilder();
+      product = await queryBuilder
+        .where('LOWER(title) =:title or slug =:slug', {
+          title: term.toLowerCase(),
+          slug: term.toLowerCase(),
+        })
+        .getOne();
     }
 
     if (!product)
