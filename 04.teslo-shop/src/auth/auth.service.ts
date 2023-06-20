72121fde556bf73699f9b05d52e15bfe60d23a6b
fersayago <sayago.fernando@gmail.com>
2023-06-20 19:54:42 -0300

    S13: Encriptar la contraseña

--- 04.teslo-shop/src/auth/auth.service.ts
+++ 04.teslo-shop/src/auth/auth.service.ts
@@ -1,6 +1,12 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+
+import * as bcrypt from 'bcrypt';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 
@@ -13,10 +19,18 @@ export class AuthService {
 
   async create(createUserDto: CreateUserDto) {
     try {
-      const user = this.userRepository.create(createUserDto);
+      const { password, ...userData } = createUserDto;
+
+      const user = this.userRepository.create({
+        ...userData,
+        password: bcrypt.hashSync(password, 10),
+      });
 
       await this.userRepository.save(user);
 
+      delete user.password;
+
+      // TODO: Retornar el JWT de acceso
       return user;
     } catch (err) {
       this.handleDBErrors(err);
