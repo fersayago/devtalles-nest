815b18a07718baf6677041666f50c033d3f70c27
fersayago <sayago.fernando@gmail.com>
2023-06-21 20:39:40 -0300

    S13: Módulos Asíncronos

--- 04.teslo-shop/src/auth/auth.module.ts
+++ 04.teslo-shop/src/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { User } from './entities/user.entity';
 
 @Module({
   controllers: [AuthController],
@@ -12,6 +14,23 @@ import { JwtModule } from '@nestjs/jwt';
   imports: [
     TypeOrmModule.forFeature([User]),
     PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        console.log(
+          'jwt secret from configService:',
+          configService.get('JWT_SECRET'),
+        );
+        // console.log('JWT SECRET', process.env.JWT_SECRET);
+        return {
+          secret: process.env.JWT_SECRET,
+          signOptions: {
+            expiresIn: '2h',
+          },
+        };
+      },
+    }),
     // JwtModule.register({
     //   secret: process.env.JWT_SECRET,
     //   signOptions: {
