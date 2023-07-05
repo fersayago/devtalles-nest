2dad922789367df275070dadc7844da8c90aa59d
fersayago <sayago.fernando@gmail.com>
2023-07-05 13:32:31 -0300

    S13: Verificar rol del usuario

--- 04.teslo-shop/src/auth/guards/user-role/user-role.guard.ts
+++ 04.teslo-shop/src/auth/guards/user-role/user-role.guard.ts
@@ -1,6 +1,13 @@
 import { Reflector } from '@nestjs/core';
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { User } from 'src/auth/entities/user.entity';
 
 @Injectable()
 export class UserRoleGuard implements CanActivate {
@@ -14,8 +21,23 @@ export class UserRoleGuard implements CanActivate {
       context.getHandler(),
     );
 
-    console.log(validRoles);
+    // si los roles no estan configurados, se deja pasar
+    if (!validRoles) return true;
+    if (validRoles.length === 0) return true;
 
-    return true;
+    const req = context.switchToHttp().getRequest();
+    const user = req.user as User;
+
+    if (!user) throw new BadRequestException('User not found');
+
+    for (const role of user.roles) {
+      if (validRoles.includes(role)) {
+        return true;
+      }
+    }
+
+    throw new ForbiddenException(
+      `User ${user.fullName} need a valid role: [${validRoles}]`,
+    );
   }
 }
